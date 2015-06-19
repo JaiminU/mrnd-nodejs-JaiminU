0f81085cb67e275a14ba68683ef5f1bbef04758c
Jaimin Upadhyay <jaiminupadhyay@hotmail.com>
2015-06-19 19:45:03 +0700

    commit changes

--- src/JSIntro.js
+++ src/JSIntro.js
@@ -1,23 +1,44 @@
 
 exports.Sum = function(num1, num2){
+	return num1+num2;
 }
 
 exports.SumOfArray = function(arrayOfNums){
-
+	var sum_of_array = 0;
+	for(i=0;i<arrayOfNums.length;i++){
+		sum_of_array += arrayOfNums[i];
+	}
+	return sum_of_array;
 }
 
 // Sum only the unique numbers in the array.
 // Ex: If array is [2,3,3,2], the sum is 2+3=5
 
 exports.SumOfUniqueNumbers = function(arrayOfNums){
+	var array = [];
+	var sum_of_array=0;
+	for(i=0;i<arrayOfNums.length;i++){
+		if(array.indexOf(arrayOfNums[i]) == -1){
+			array.push(arrayOfNums[i]);
+		}
+	}
+	for(i=0;i<array.length;i++){
+		sum_of_array+=array[i];
+	}
+	return sum_of_array;
 }
 
 exports.ReverseString = function(str){
-
+	return str.split('').reverse().join('');
 }
 
 
 exports.ReverseArrayOfStrings = function(arrayOfStrings){
-
-
+	JSIntro = require("./JSIntro.js")
+	var reversed_strings = [];
+	for(i=0;i<arrayOfStrings.length;i++){
+		//reversed_strings.push(arrayOfStrings[i].split('').reverse().join(''));
+		reversed_strings.push(JSIntro.ReverseString(arrayOfStrings[i]));
+	}
+	return reversed_strings;
 }
--- src/contacts/routes/contacts.js
+++ src/contacts/routes/contacts.js
@@ -1,17 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var contacts = [];
 
 /* GET contacts */
 router.get('/:id', function(req, res, next) {
+  res.send(contacts[req.params.id]);
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
+  //console.log("From contacts: " + req.body);
+  req.body.messages = [];
+  contacts.push(req.body);
+  res.send('' + (contacts.length - 1));
 });
 
 router.put('/:id', function(req, res, next) {
-  console.log(req.body);
+  //console.log("Update: " + JSON.stringify(req.body));
+  var id = req.params.id;
+  for(var prop in req.body){
+    contacts[id][prop] = req.body[prop];
+  }
+  res.json(contacts[id]);
+});
 
+router.post('/:id/message', function(req,res,next){
+  var id = req.params.id;
+  var msg_id = contacts[id].messages.length;
+  contacts[id].messages[msg_id] = req.body;
+  //console.log(contacts[id]);
+  res.send('' + msg_id);
+});
+
+router.get('/:id/message/:msg_id', function(req,res,next){
+  var id = req.params.id;
+  var msg_id = req.params.msg_id;
+  //console.log("Id: " + id + " Message Id: " + msg_id);
+  res.json(contacts[id].messages[msg_id]);
 });
 
 module.exports = router;
